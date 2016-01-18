d16f89cb98699e8c77cdf87d97754fa5625f746c
NinjaNionios <NinjaNionios@Ninja-PC>
2016-01-18 12:37:41 +0200

    First commit !! xD

---
+++ src/Login.java
@@ -0,0 +1,69 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+
+public class Login {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login window = new Login();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Login() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 600, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(258, 91, 86, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("\u038C\u03BD\u03BF\u03BC\u03B1");
+		lblNewLabel.setBounds(186, 97, 46, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("\u039A\u03C9\u03B4\u03B9\u03BA\u03CC\u03C2");
+		lblNewLabel_1.setBounds(186, 133, 46, 14);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(258, 130, 85, 20);
+		frame.getContentPane().add(passwordField);
+		
+		JButton button = new JButton("\u03A3\u03CD\u03BD\u03B4\u03B5\u03C3\u03B7");
+		button.setBounds(218, 173, 89, 23);
+		frame.getContentPane().add(button);
+	}
+}
